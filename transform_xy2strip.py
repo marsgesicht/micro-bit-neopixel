3c8b095c90d275ca7c4546465d0ed282486b488c
pek <marsgesicht@gmail.com>
2019-12-14 14:28:13 +0100

    with each new puddle cycle random amount of drips and drip size is choosen

--- transform_xy2strip.py
+++ transform_xy2strip.py
@@ -8,18 +8,19 @@ MAX_Y = 5
 LED_COUNT = 59
 LED_PIN = pin0
 STRIP = neopixel.NeoPixel(LED_PIN, LED_COUNT+1)
-SENSE = 20
+SENSE = 25
 
 def drip(strip, drip_count=3, drip_size=10):
     y = MAX_Y - 1
     x_list = []
-    for j in range(drip_count):
-        x_list.append([randint(0,MAX_X-1),0])
     weight = 0
     puddle_x = 0
+    def x_list_init(dc):
+        for i in range(dc):
+            x_list.append([randint(0,MAX_X-1),0])
     def puddle(w=10):
+        puddle_x += w
         for i in range (MAX_X):
-            puddle_x += w/MAX_X
             p_x = math.floor(puddle_x)
             if (p_x<255):
                 strip[transform(i,0)] = (0,randint(0,20),(p_x&255))
@@ -28,11 +29,19 @@ def drip(strip, drip_count=3, drip_size=10):
             else:
                 puddle_x = 0
                 strip[transform(i,0)] = (0,0,0)
+        if puddle_x == 0:
+            nonlocal drip_count, drip_size
+            drip_count = randint(4,MAX_X)  
+            drip_size = randint(5,50)
+            x_list.clear()
     while True:
         for j in range(drip_count):
+            if len(x_list) == 0:
+                x_list_init(drip_count)
+                break
             x = x_list[j][0]
             mo_detect(accelerometer.get_values()) 
-            x_list[j][1] = x_list[j][1] + (j+1)/randint(9,19)     #weight of the drip => dripping speed
+            x_list[j][1] = x_list[j][1] + (j+1)/randint(9,19)
             weight = x_list[j][1]
             if (weight<=85):
                 strip[transform(x,y)] = (int(weight*3),int(85-weight),0)
@@ -40,22 +49,19 @@ def drip(strip, drip_count=3, drip_size=10):
                 x_list[j][1] = 0
                 x_list[j][0] = randint(0,MAX_X-1)
                 strip[transform(x,y)] = (0,0,0)
-                for iy in range(y-1,-1,-1):     #falling drip animation
+                for iy in range(y-1,-1,-1):
                     strip[transform(x,iy+1)] = (0,0,0)
                     strip[transform(x,iy)] = (int(51*iy),0,int(randint(175,255)/(iy+1)))
                     strip.show()
-                puddle(drip_size)                    #fill puddle
+                puddle(drip_size)
             strip.show()
 def transform(x,y):
-    #grid x/y is 12/5
     x = x % MAX_X
     y = y % MAX_Y
-    s = x * MAX_Y   #strip
-    if (x/2 == x//2) | (x == 0):   #x is even -> y(0-4) 
-        #s += y
-        s += MAX_Y-1-y 
-    else:                          #x is odd ->y(4-0)
-        #s += MAX_Y-1-y
+    s = x * MAX_Y 
+    if (x/2 == x//2) | (x == 0): 
+        s += MAX_Y-1-y
+    else:
         s += y
     return s
 def wheel(pos):
@@ -71,14 +77,12 @@ def wheel(pos):
 
 def display_image(strip):
     heart = ((2,0),(1,1),(2,1),(3,1),(0,2),(1,2),(2,2),(3,2),(4,2),(0,3),(1,3),(2,3),(3,3),(4,3),(1,4),(3,4))
-    smiley = ((1,0),(2,0),(3,0),(0,1),(4,1),(2,2),(1,3),(3,3))
     _n = ((0,0),(4,0),(0,1),(3,1),(4,1),(0,2),(2,2),(4,2),(0,3),(1,3),(4,3),(0,4),(4,4))
     _g = ((0,0),(1,0),(2,0),(3,0),(4,0),(0,1),(4,1),(0,2),(3,2),(4,2),(0,3),(1,4),(2,4),(3,4))
     _o = ((2,0),(1,1),(2,1),(3,1),(0,2),(4,2),(0,3),(4,3),(1,4),(2,4),(3,4))
     _c = ((1,0),(2,0),(3,0),(4,0),(0,1),(0,2),(0,3),(1,4),(2,4),(3,4),(4,4))
-    images = (heart,) if randint(1,8)<7 else (heart,_n,_g,_o,_c,heart,smiley)
+    images = (heart,) if randint(1,8)<7 else (heart,_n,_g,_o,_c,heart,)
     for k in range(len(images)):
-        #sleep(500)
         strip.clear()
         image = images[k]
         s_list = []
@@ -95,26 +99,14 @@ def display_image(strip):
                 strip[i] = (0,0,0)
             s_list.clear()
         strip.clear()
-def mo_detect(xyz_init,sense=30):  #xyz_init = accelerometer.get_values()
+def mo_detect(xyz_init,sense=30):
     s = sense
     (x_init,y_init,z_init) = xyz_init
     (x_,y_,z_) = accelerometer.get_values()
     if (x_init-s>x_) | (x_init+s<x_) | (y_init-s>y_) | (y_init+s<y_) | (z_init-s>z_) | (z_init+s<z_):
         display_image(STRIP)
-        
 if __name__ == '__main__':
     xyz_init = accelerometer.get_values()
     while True:
         display.clear()  
-        drip(STRIP,5)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        drip(STRIP,10,220)
